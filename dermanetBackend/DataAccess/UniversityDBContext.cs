be9198d9bd8cda547c501ecebee17f8e422e0daa
mornaeldernar <jimenez.rafa@gmail.com>
2022-09-21 13:41:50 -0500

    First Commit

---
+++ dermanetBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,17 @@
+﻿using dermanetBackend.Models.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace dermanetBackend.DataAccess
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options)
+        {
+
+        }
+
+        // Add DbSets (tablas of our data base)
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Curso>? Cursos { get; set; }
+    }
+}
